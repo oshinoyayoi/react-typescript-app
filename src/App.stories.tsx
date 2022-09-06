85b6a4fab43de50f9beac3672dc75a8ca1573411
forEngineerCo <jy905384525@yahoo.co.jp>
2022-09-06 14:31:06 +0900

    test stories

---
+++ src/App.stories.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import App from './App';
+
+export default {
+    title: 'Example/App',
+    component: App,
+    // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+    argTypes: {
+        backgroundColor: { control: 'color' },
+    },
+} as ComponentMeta<typeof App>;
+
+const Template: ComponentStory<typeof App> = () => <App />;
+
+export const PrimaryTest = Template.bind({});
+
+PrimaryTest.args = {
+    primary: true,
+    label: 'Button',
+};
